a3047ce101d7dae5f674bf66ca940485b4cac76a
pontakornth <pontakorn61@gmail.com>
2020-06-26 21:07:57 +0700

    content: Add more words

--- src/utils/dict.js
+++ src/utils/dict.js
@@ -1,6 +1,14 @@
 const dictionary = new Map([
   [/\s?approach\s?/gi, 'วิธีการ'],
   [/\s?machine learning\s?/gi, 'การเรียนรู้ของเครื่อง'],
+  [/\s?big data\s?/gi, 'ข้อมูลมหัต'],
+  [/\s?coding\s?/gi, 'การกำหนดรหัส'],
+  [/\s?joystick\s?/gi, 'แท่งหรรษา'],
+  [/\s?subscriber\s?/gi, 'ผู้รับบริการ'],
+  [/\s?flash mob\s?/gi, 'ม็อบฉับพลัน'],
+  [/\s?red ocean\s?/gi, 'มหาสมุทรสีชาด'],
+  [/\s?blue ocean\s?/gi, 'มหาสมุทรสีคราม'],
+  [/\s?communicate\s?/gi, 'สื่อสาร'],
 ]);
 
 export default dictionary;
