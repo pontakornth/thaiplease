9d2871aba14ea943bc7d6452d938821f6cf571a8
pontakornth <pontakorn61@gmail.com>
2020-06-26 11:10:55 +0700

    feat: MVP Mockup

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,47 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="appbar">
+      <h1>ThaiPlease</h1>
+    </div>
+    <div class="input-form">
+      <label>ใส่ข้อควาามไทย-อังกฤษที่นี่</label>
+      <textarea class="input" rows="10" placeholder="Big data" />
+    </div>
+    <div class="button-group">
+      <button class="button is-primary">แปลภาษาไทย</button>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
-
 export default {
   name: 'app',
-  components: {
-    HelloWorld,
-  },
 };
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="postcss">
+.appbar {
+  @apply text-center text-white bg-blue-400 text-5xl;
+}
+.input {
+  @apply p-4 bg-gray-100 text-black border w-full;
+}
+.input-form {
+  @apply p-4;
+}
+.input-form label {
+  @apply my-4 block font-display text-3xl;
+}
+.button-group {
+  @apply py-4 px-8 flex justify-center items-center text-center;
+}
+.button {
+  @apply p-2 cursor-pointer border font-display;
+}
+.button.is-primary {
+  @apply text-white bg-blue-400;
+}
+.button.is-primary:hover {
+  @apply bg-blue-500;
 }
 </style>
