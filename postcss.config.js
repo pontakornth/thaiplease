465d97d7a3790d701ba5a9a217b996679386301e
pontakornth <pontakorn61@gmail.com>
2020-06-26 16:59:20 +0700

    chore: Add purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,22 @@
+/* eslint-disable global-require */
 /* eslint-disable import/no-extraneous-dependencies */
 const autoprefixer = require('autoprefixer');
 const tailwindcss = require('tailwindcss');
+
+const IN_PRODUCTION = process.env.NODE_ENV === 'production';
 
 module.exports = {
   plugins: [
     tailwindcss(),
     autoprefixer(),
+    IN_PRODUCTION && require('@fullhuman/postcss-purgecss')({
+      content: ['./public/**/*.html', './src/**/*.vue'],
+      defaultExtractor(content) {
+        const contentWithoutStyleBlocks = content.replace(/<style[^]+?<\/style>/gi, '');
+        return contentWithoutStyleBlocks.match(/[A-Za-z0-9-_/:]*[A-Za-z0-9-_/]+/g) || [];
+      },
+      whitelist: [],
+      whitelistPatterns: [/-(leave|enter|appear)(|-(to|from|active))$/, /^(?!(|.*?:)cursor-move).+-move$/, /^router-link(|-exact)-active$/, /data-v-.*/],
+    }),
   ],
 };
